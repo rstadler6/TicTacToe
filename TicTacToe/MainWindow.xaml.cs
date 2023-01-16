4370b501ad41fd0bbeb7d5ae9f856732cb47047e
Raphael Stadler <raphael.stadler@cmiag.ch>
2023-01-16 14:02:20 +0100

    initial commit

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private string[,] board = new string[3, 3];
+        private bool player1Turn = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = "";
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int row = Grid.GetRow(button);
+            int col = Grid.GetColumn(button);
+
+            if (board[row, col] != "")
+            {
+                MessageBox.Show("This cell is already occupied!");
+                return;
+            }
+
+            if (player1Turn)
+            {
+                button.Content = "X";
+                board[row, col] = "X";
+                player1Turn = false;
+            }
+            else
+            {
+                button.Content = "O";
+                board[row, col] = "O";
+                player1Turn = true;
+            }
+
+            if (IsWin())
+            {
+                MessageBox.Show("Player " + (player1Turn ? "2" : "1") + " wins!");
+                ResetGame();
+            }
+            else if (IsDraw())
+            {
+                MessageBox.Show("It's a draw!");
+                ResetGame();
+            }
+        }
+
+        private bool IsWin()
+        {
+            // Check rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != "")
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != "")
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != "")
+            {
+                return true;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != "")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsDraw()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "")
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private void ResetGame()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = "";
+                }
+            }
+
+            player1Turn = true;
+
+            button1.Content = "";
+            button2.Content = "";
+            button3.Content = "";
+            button4.Content = "";
+            button5.Content = "";
+            button6.Content = "";
+            button7.Content = "";
+            button8.Content = "";
+            button9.Content = "";
+        }
+    }
+}
